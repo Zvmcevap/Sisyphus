945cc794c48c1b10b9af012957b367fd35d3b702
Zvmcevap <zupanc.beno@gmail.com>
2020-11-13 12:19:46 +0100

    Starting out

---
+++ schedule.py
@@ -0,0 +1,11 @@
+class Weekly:
+    def __init__(self, onesie_id):
+        self.weekly_id = 0
+        self.onesie_id = onesie_id
+        self.monday = 0
+        self.tueday = 0
+        self.wednesday = 0
+        self.thursday = 0
+        self.friday = 0
+        self.saturday = 0
+        self.sunday = 0
